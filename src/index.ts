5a470fca0b9506f63293ac1e00df45ede1a34301
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-03-16 22:25:12 +0200

    Added footer

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,15 @@ const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
 const reviewTotalDisplay = document.querySelector("#reviews");
 let isOpen: boolean;
-const propertyContainer = document.querySelector('.properties')
+const propertyContainer = document.querySelector(".properties");
+const footer = document.querySelector(".footer");
 
 const reviews: {
-  name:string;
+  name: string;
   stars: number;
   loyaltyUser: boolean;
   date: string;
-} [] = [
+}[] = [
   {
     name: "Sheia",
     stars: 5,
@@ -59,7 +60,7 @@ const you: {
   lastName: "Bhendane",
   isReturning: true,
   age: 28,
-  stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow'],
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 
 function populateUser(isReturning: boolean, userName: string) {
@@ -73,66 +74,75 @@ populateUser(you.isReturning, you.firstName);
 
 // Properties
 
-const properties : {
+const properties: {
   image: string;
   title: string;
   price: number;
   location: {
-      firstLine: string;
-      city: string;
-      code: number;
-      country: string;
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
   };
-  contact: string;
+  contact: [number, string];
   isAvailable: boolean;
 }[] = [
   {
-      image: 'images/colombia-property.jpg',
-      title: 'Colombian Shack',
-      price: 45,
-      location: {
-          firstLine: 'shack 37',
-          city: 'Bogota',
-          code: 45632,
-          country: 'Colombia'
-      },
-      contact: 'marywinkle@gmail.com',
-      isAvailable: true  
+    image: "images/colombia-property.jpg",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: "marywinkle@gmail.com",
+    isAvailable: true,
   },
   {
-      image: 'images/poland-property.jpg',
-      title: 'Polish Cottage',
-      price: 34,
-      location: {
-          firstLine: 'no 23',
-          city: 'Gdansk',
-          code: 343903,
-          country: 'Poland'
-      },
-      contact: 'garydavis@hotmail.com',
-      isAvailable: false 
+    image: "images/poland-property.jpg",
+    title: "Polish Cottage",
+    price: 34,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: "garydavis@hotmail.com",
+    isAvailable: false,
   },
   {
-      image: 'images/london-property.jpg',
-      title: 'London Flat',
-      price: 23,
-      location: {
-          firstLine: 'flat 15',
-          city: 'London',
-          code: 35433,
-          country: 'United Kingdom',
-      },
-      contact: 'andyluger@aol.com',
-      isAvailable: true
-  }
-]
+    image: "images/london-property.jpg",
+    title: "London Flat",
+    price: 23,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: 35433,
+      country: "United Kingdom",
+    },
+    contact: "andyluger@aol.com",
+    isAvailable: true,
+  },
+];
 
 for (let i = 0; i < properties.length; i++) {
-  const card = document.createElement('div')
-  card.classList.add('card')
-  card.innerHTML = properties[i].title
-  const image = document.createElement('img')
-  image.setAttribute('src', properties[i].image)
-  card.appendChild(image)
-  propertyContainer.appendChild(card)
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = properties[i].title;
+  const image = document.createElement("img");
+  image.setAttribute("src", properties[i].image);
+  card.appendChild(image);
+  propertyContainer.appendChild(card);
 }
+
+let currentLocation: [string, string, number] = ["London", "11:35", 17];
+footer.innerHTML =
+  currentLocation[0] +
+  " " +
+  currentLocation[1] +
+  " " +
+  currentLocation[2] +
+  "°";
